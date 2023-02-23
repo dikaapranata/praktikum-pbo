cfbc7cf056f834296b3c4528711c6238fd424250
Handhika Pranata <93708899+dikaapranata@users.noreply.github.com>
2023-02-23 23:39:39 +0700

    Add files via upload

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,42 @@
+/*
+	Titik.java  23/02/2023
+	Nama      : Handhika Pranata Kusuma Wardana
+	NIM       : 24060121120012
+	Deskripsi : kelas untuk titik
+*/
+
+public class Titik{
+	//atribut
+	double absis;
+	double ordinat;
+	static int counterTitik;
+	
+	//konstruktor
+	Titik(){
+		absis = 0;
+		ordinat = 0;
+		counterTitik++;
+	}  
+	Titik(double a, double o){
+		absis = a;
+		ordinat = o;
+		counterTitik++;
+	}
+	
+	//metode
+	void setAbsis(double a){
+		absis =a;
+	}
+	double getAbsis(){
+		return absis;
+	}
+	void setOrdinat(double b){
+		ordinat = b;
+	}
+	double getOrdinat(){
+		return ordinat;
+	}
+	int getCounterTitik(){
+		return counterTitik;
+	}
+}
