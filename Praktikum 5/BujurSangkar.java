15ec8629701f3576e91e26b1500215842b5d368e
Handhika Pranata <93708899+dikaapranata@users.noreply.github.com>
2023-03-29 09:59:12 +0700

    Create BujurSangkar.java

---
+++ Praktikum 5/BujurSangkar.java
@@ -0,0 +1,16 @@
+/**
+* File		    : BujurSangkar.java 25/03/2023
+* Nama / NIM	: Handhika Pranata Kusuma Wardana / 24060121140112
+* Deskripsi	  : Kelas yang membuat implementasi metode abstrak pada bangun datar
+*/
+
+public class BujurSangkar extends BangunDatar{
+	//apabila kelas BujurSangkar tidak membuat implementasi metode abstrak yang ada pada kelas BangunDatar 
+	//maka kelas BujurSangkar juga harus dideklarasikan sebagai kelas abstrak. 
+	//Ini karena, kelas BujurSangkar akan mewarisi metode abstrak dari kelas induknya (BangunDatar) 
+	//tetapi tidak memberikan implementasi untuk metode abstrak tersebut.
+	public double hitungLuas(double sisi){
+		luas = sisi*sisi;
+		return luas;
+	}
+}
