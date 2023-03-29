6f86e5e963670b3420a7388dad6bc3a08bc1bbdc
Handhika Pranata <93708899+dikaapranata@users.noreply.github.com>
2023-03-29 09:52:45 +0700

    Update IArea.java

--- Praktikum 5/IArea.java
+++ Praktikum 5/IArea.java
@@ -1,9 +1,9 @@
 /**
-* File 		    : IArea.java 26-03-2023
+* File 		: IArea.java 26-03-2023
 * Nama / NIM	: Handhika Pranata Kusuma Wardana / 24060121140112
 * Deskripsi   	: Interface yang mendefinisikan kontrak bahwa setiap kelas 
-			      yang membuat implementasi dari interface ini harus memiliki cara
-			      untuk menghitung luas
+                  yang membuat implementasi dari interface ini harus memiliki cara
+	          untuk menghitung luas
 */
 
 public interface IArea{
