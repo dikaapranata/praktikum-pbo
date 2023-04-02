f1b2f0875dc8a15c9b86ff44b1142e255efb3d09
Handhika Pranata <93708899+dikaapranata@users.noreply.github.com>
2023-04-02 15:11:24 +0700

    Add files via upload

---
+++ Praktikum 6/AngkaSialException.java
@@ -0,0 +1,13 @@
+/*
+
+* File		    : AngkaSialException.java 02-04-2023
+* Nama / NIM    : Handhika Pranata Kusuma Wardana / 24060121140112
+* Deskripsi  	: Eksepsi buatan sendiri, menolak masukan angka 13!
+
+*/
+
+public class AngkaSialException extends Exception{
+	public AngkaSialException(){
+		super("jangan memasukkan angka 13 karena angka sial !!!");
+	}
+}
---
+++ Praktikum 6/ExceptionOnArray.java
@@ -0,0 +1,22 @@
+/*
+
+* File		    : ExceptionOnArray.java 02-04-2023
+* Nama / NIM    : Handhika Pranata Kusuma Wardana / 24060121140112
+* Deskripsi   	: Program penggunaan eksepsi menggunakan class library Java
+
+*/
+
+public class ExceptionOnArray{
+	public static void main(String[] args){
+		//instansiasi object array integer
+		Integer[] arrayInteger = new Integer[4];
+		try{
+			arrayInteger[2] = 11;
+			arrayInteger[4] = 10;
+		}catch(ArrayIndexOutOfBoundsException exception){
+			exception.printStackTrace();
+		}finally{
+			System.out.println("clean up code...");
+		}
+	}
+}
